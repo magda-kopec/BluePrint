e436d3cf8346b2adba4c0eac51728e6b930f805e
Magdalena Kopec <magdalena.kopec@aldi-techhub.pl>
2024-09-08 16:03:17 +0200

    Initial Commit

---
+++ BluePrint/BluePrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluePrintApp.swift
+//  BluePrint
+//
+//  Created by Magdalena Kopec on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BluePrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
